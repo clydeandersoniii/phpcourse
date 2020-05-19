e6cccd8570b633323d85b787a9a41d30d4589447
Clyde Anderson <clyde.anderson.e@gmail.com>
2020-05-19 16:32:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    </br>
+    <center>
+        <h1>
+            <?php
+                //Print hello world
+                echo "Hello World!</br>";
+                $firstName = "Clyde";
+                $favNumber = 31;
+
+                echo "<h2>" . $firstName . "</h2>";
+            ?>
+        </h1>
+    </center>
+ </body>
+</html>
